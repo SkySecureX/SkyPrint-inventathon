6e65869e84cbc6284c842c45a2a3e66ccccd48b6
Saran Chockan <saranjitharunesh@gmail.com>
2018-04-14 14:45:41 -0500

    created Printer UI

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,14 +1,25 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import com.jfoenix.controls.JFXButton;
+
+import java.io.IOException;
 import java.net.URL;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
     JFXButton start;
+
+    Stage PrinterUI;
+    Parent PrinterRoot = null;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -18,6 +29,20 @@ public class Controller implements Initializable {
 
     @FXML
     public void getStarted(ActionEvent event) throws Exception{
+
+        PrinterUI = new Stage();
+        try {
+            PrinterRoot = FXMLLoader.load(getClass().getResource("PrinterUI.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        PrinterUI.setTitle("SkyPrint");
+        PrinterUI.setScene(new Scene(PrinterRoot,700,600));
+        PrinterUI.show();
+
+        Stage sourceStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        sourceStage.close();
 
     }
 }

d7cb4b674706470028a62c5abac3a9b666c08477
Saran Chockan <saranjitharunesh@gmail.com>
2018-04-14 13:58:16 -0500

    Initial Commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,25 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import javafx.event.ActionEvent;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    JFXButton start;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    @FXML
+    public void getStarted(ActionEvent event) throws Exception{
+
+    }
+}
+
+
